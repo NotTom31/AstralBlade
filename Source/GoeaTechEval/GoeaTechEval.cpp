e5cfe92642996a3b9c2717ec0c9bb6ef55cfc9a1
Thomas Mezzie <99389013+NotTom31@users.noreply.github.com>
2024-01-19 14:07:19 -0800

    Removed Eval code now that there's no dependencies on it

--- Source/GoeaTechEval/GoeaTechEval.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "GoeaTechEval.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GoeaTechEval, "GoeaTechEval" );
- 
